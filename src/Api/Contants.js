4a41d678ff9a3ac4f54ca269183ddf7ae46e2701
gvidal <gvidal@frba.utn.edu.ar>
2021-05-05 19:45:50 -0300

    Axios singleton & Api dir

--- src/Api/Contants.js
+++ src/Api/Contants.js
@@ -1,9 +1,15 @@
-export const GET_USER_BY_MAIL_ENDPOINT = 'api/users/';
-export const GET_MENU_BY_BRANDNAME_ENDPOINT = 'api/menu/';
-export const CREATE_USER_ENDPOINT = 'api/users';
-export const UPDATE_USER_MENU_ENDPOINT = 'api/users/';
-export const UPDATE_USER_DATA_ENDPOINT = 'api/users/';
-export const PAYKU_CONTROLLER_ENDPOINT = 'api/payku/';
+export const GET_USER_BY_MAIL_ENDPOINT = 'http://localhost:80/api/v1/users/';
+export const GET_MENU_BY_BRANDNAME_ENDPOINT = 'http://localhost:80/api/v1/menu/';
+export const CREATE_USER_ENDPOINT = 'http://localhost:80/api/v1/users';
+export const UPDATE_USER_MENU_ENDPOINT = 'http://localhost:80/api/v1/users/';
+export const UPDATE_USER_DATA_ENDPOINT = 'http://localhost:80/api/v1/users/';
+export const PAYKU_CONTROLLER_ENDPOINT = 'http://localhost:80/api/v1/payku/';
+// export const GET_USER_BY_MAIL_ENDPOINT = 'api/users/';
+// export const GET_MENU_BY_BRANDNAME_ENDPOINT = 'api/menu/';
+// export const CREATE_USER_ENDPOINT = 'api/users';
+// export const UPDATE_USER_MENU_ENDPOINT = 'api/users/';
+// export const UPDATE_USER_DATA_ENDPOINT = 'api/users/';
+// export const PAYKU_CONTROLLER_ENDPOINT = 'api/payku/';
 
 export const NEW_SUSCRIPTION = {
     "clientId": null,
@@ -13,7 +19,7 @@ export const NEW_SUSCRIPTION = {
 
 export const NEW_USER = {
     "address": "Ingrese una direccion",
-    "brandName": "menumenu",
+    "brandName": null,
     "categoryId": 1,
     "deliveryCharge": 0,
     "description": "Ingrese una description",
