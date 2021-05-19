78cb9d50c9269168b984b19aa9a62f1cdff5e573
gvidal <gvidal@frba.utn.edu.ar>
2021-05-18 22:59:22 -0300

    Delete broken js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -22,23 +22,23 @@ $(function () {
 
 
 
-    //===== Section Menu Active
-
-    var scrollLink = $('.page-scroll');
-    // Active link switching
-    $(window).scroll(function () {
-        var scrollbarLocation = $(this).scrollTop();
-
-        scrollLink.each(function () {
-
-            var sectionOffset = $(this.hash).offset().top - 73;
-
-            if (sectionOffset <= scrollbarLocation) {
-                $(this).parent().addClass('active');
-                $(this).parent().siblings().removeClass('active');
-            }
-        });
-    });
+    // //===== Section Menu Active
+    //
+    // var scrollLink = $('.page-scroll');
+    // // Active link switching
+    // $(window).scroll(function () {
+    //     var scrollbarLocation = $(this).scrollTop();
+    //
+    //     scrollLink.each(function () {
+    //
+    //         var sectionOffset = $(this.hash).offset().top - 73;
+    //
+    //         if (sectionOffset <= scrollbarLocation) {
+    //             $(this).parent().addClass('active');
+    //             $(this).parent().siblings().removeClass('active');
+    //         }
+    //     });
+    // });
 
 
     //===== close navbar-collapse when a  clicked
